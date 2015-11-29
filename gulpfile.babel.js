3531f66425292e1f687b4685e8e1c1215f706a3e
Joe Billingsley <jb@josephbillingsley.co.uk>
2015-11-29 22:16:17 +0000

    Added handlebar templates. Corrected various issues.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,33 @@
 'use strict';
-
+import fs from 'fs';
 import gulp from 'gulp';
 import del from 'del';
+import browserSync from 'browser-sync';
 import gulpLoadPlugins from 'gulp-load-plugins';
 
 const $ = gulpLoadPlugins();
 
 const paths = {
-  src: '',
-  dest: 'build/',
+  src: 'src/',
+  dest: 'build/'
 }
 
+var reload = browserSync.reload;
+
 var src = {
+  templates: paths.src + 'templates/',
   scripts: paths.src + "scripts/",
+  data: paths.src + 'data/',
   styles: paths.src + "styles/",
-  images: paths.src + "img/"
+  images: paths.src + "img/",
+  html: paths.src
 }
 
 var dest = {
-  scripts: paths.dest + "scripts/",
-  styles: paths.dest + "styles/",
-  images: paths.src + "img/",
+  scripts: paths.dest,
+  styles: paths.dest,
+  images: paths.dest + "img/",
+  html: paths.dest
 }
 
 gulp.task('clean', () => {
@@ -34,33 +41,50 @@ gulp.task('lint', () => {
     .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('html', () => {
-  return gulp.src(paths.src + '**/*.html')
-    .pipe($.minifyHtml())
-    .pipe(gulp.dest(paths.dest));
+gulp.task('handlebars', () => {
+  var data = JSON.parse(
+    fs.readFileSync(src.data + "/data.json"));
+    
+  var options = {
+    batch : [src.templates],
+  }
+  
+  return gulp.src(src.html + '/*.handlebars')
+    .pipe($.compileHandlebars(data, options))
+    .pipe($.rename((path) => {
+      path.extname = ".html"
+    }))
 });
 
-gulp.task('scripts', ['lint', 'clean'], () => {
+gulp.task('html', ['handlebars'], () => {
+  return gulp.src(src.html + '**/*.html')
+    .pipe($.minifyHtml())
+    .pipe($.size({ title: 'html' }))
+    .pipe(gulp.dest(dest.html))
+});
+
+gulp.task('scripts', ['lint'], () => {
   return gulp.src(src.scripts)
     .pipe($.sourcemaps.init())
       .pipe($.babel())
     .pipe($.sourcemaps.write())
       .pipe($.concat('main.min.js'))
       .pipe($.uglify())
-      .pipe($.size({title: 'scripts'}))
+      .pipe($.size({ title: 'scripts' }))
     .pipe($.sourcemaps.write())
+    .pipe($.size({ title: 'scripts' }))
     .pipe($.rev())
     .pipe(gulp.dest(dest.scripts));
 });
 
-gulp.task('styles', ['clean'], () => {
+gulp.task('styles', () => {
   return gulp.src([
       src.styles + "**/*.sass",
       src.styles + "**/*.css"
     ])
     .pipe($.sourcemaps.init())
       .pipe($.sass()).on('error', $.sass.logError)
-      .pipe($.autoprefixer( {
+      .pipe($.autoprefixer({
         browsers: ['last 2 versions']
       }))
       .pipe($.minifyCss())
@@ -68,28 +92,39 @@ gulp.task('styles', ['clean'], () => {
         html: ['**/*.html']
       }))
       .pipe($.concat('main.styles.css'))
-      .pipe($.size({title: 'styles'}))
     .pipe($.sourcemaps.write())
+    .pipe($.size({ title: 'styles' }))
     .pipe($.rev())
     .pipe(gulp.dest(dest.styles))
+    .pipe(browserSync.stream())
 });
 
-gulp.task('images', ['clean'], () => {
+gulp.task('images', () => {
   return gulp.src(src.images + '**/*')
     .pipe($.cache($.imagemin({
-        optimisationLevel: 5,
-        progressive: true,
-        interlaced: true
+      optimisationLevel: 5,
+      progressive: true,
+      interlaced: true
     })))
+    .pipe($.size({ title: 'images' }))
     .pipe($.rev())
     .pipe(gulp.dest(dest.images))
-    .pipe($.size({title: 'images'}))
+    .pipe(browserSync.stream())
 });
 
 gulp.task('watch', () => {
-  gulp.watch(src.scripts, ['scripts']);
-  gulp.watch(src.styles, ['styles']);
-  gulp.watch(src.images, ['images']);
+  browserSync({
+    notify: false,
+    logPrefix: 'IDC',
+    scrollElementMapping: ['main', '.mdl-layout'],
+    server: ['build'],
+    port: 3000
+  });
+
+  gulp.watch(src.html + '**/*.html', ['html', reload]);
+  gulp.watch(src.scripts + '**/*.js', ['scripts', reload]);
+  gulp.watch([src.styles + '**/*.(sass|css)'], ['styles']);
+  gulp.watch(src.images + '**/*', ['images']);
 });
 
-gulp.task('default', ['scripts', 'styles', 'images', 'watch']);
+gulp.task('default', ['clean', 'html', 'scripts', 'styles', 'images', 'watch']);
